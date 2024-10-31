796e15bf1047461ad5c5c6864c4eabd7917e1cde
floresrf <areneflores10@hotmail.com>
2024-10-31 11:37:37 -0700

    first commit

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace libreria_ARFR.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext()
+        {
+
+        }
+    }
+}
